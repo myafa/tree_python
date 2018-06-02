20c80db2dd675a3bd196a1edb7c558d15cafb575
myafa <821koesnow@gmail.com>
2018-06-02 22:10:16 +0900

    binary search tree class is defined

---
+++ BST.py
@@ -0,0 +1,24 @@
+class BinarySearchTree:
+    root = ''
+   
+    def __init__(self):
+        pass
+    def insert(self, value, node=''):
+	pass
+    def search(self, value, node=''):
+	pass
+    def delete(self, value, node=''):
+	pass
+    def findMax(self, value, node=''):
+	pass
+    def findMin(self, value, node=''):
+	pass
+    def traverseLevelOrder(self):
+  	pass
+    def traverseInOrder(self, value, node=''):
+	pass
+    def traversePreOrder(self, value, node=''):
+	pass
+    def traversePostOrder(self, value, node=''):
+	pass
+    
