eeebc258ac927018442463903bf5b38a8261b3f7
myafa <821koesnow@gmail.com>
2018-06-02 21:46:37 +0900

    tree node class defined

---
+++ tree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    nodeLHS = ''
+    nodeRHS = ''
+    nodeParent = ''
+    value = ''
+
+    def __init__(self, value, nodeParent):
+    	self.value = value
+	self.nodeParent = nodeParent
+    def getLHS(self):
+  	return self.nodeLHS
+    def getRHS(self):
+	return self.nodeRHS
+    def getValue(self):
+	return self.value
+    def setLHS(self,LHS):
+	self.nodeLHS = LHS
+    def setRHS(self,RHS):
+	self.nodeRHS = RHS
+    def setValue(self,value):
+	self.value=value
+    def setParent(self, nodeParent):
+	self.nodeParent = nodeParent
+
